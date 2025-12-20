36e62a6bbdc1ff29830e57555e5202551b1248cb
notsenpai52013-bit <notsenpai52013@gmail.com>
2025-12-20 21:41:50 +0500

    Update skin URL in content.js

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 let enabled = false;
-let skinURL = 'https://example.com/my_skin.png'; // Replace with your PNG URL
+let skinURL = 'https://t.novaskin.me/f86fda253940b102add31469dcc0167a3183d850388856b3111aed490fa7e14a'; // Replace with your PNG URL
 
 function applySkin() {
     if (window.player && window.player.setSkin) {
