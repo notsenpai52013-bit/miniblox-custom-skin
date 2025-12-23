887ad17a7ea5f12f8d00445c69023b56e89cad9e
notsenpai52013-bit <notsenpai52013@gmail.com>
2025-12-23 14:02:02 +0500

    Update CUSTOM_SKIN_URL to a new link

--- content.js
+++ content.js
@@ -2,7 +2,7 @@ function injectInto(win) {
   const script = win.document.createElement('script');
   script.textContent = `
     (() => {
-      const CUSTOM_SKIN_URL = 'https://YOUR_WORKING_SKIN_LINK';
+      const CUSTOM_SKIN_URL = 'https://t.novaskin.me/f86fda253940b102add31469dcc0167a3183d850388856b3111aed490fa7e14a';
       let USED = false;
 
       console.log('[Miniblox EXT] Injected');
