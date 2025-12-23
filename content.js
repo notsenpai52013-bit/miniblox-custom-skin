474cd5df3d67de9a2299163b26ebcceaef20cd69
notsenpai52013-bit <notsenpai52013@gmail.com>
2025-12-23 14:01:33 +0500

    Implement image skin injection in iframe
    Added a function to inject a custom skin into images within an iframe.

--- content.js
+++ content.js
@@ -1,4 +1,44 @@
-const script = document.createElement('script');
-script.src = chrome.runtime.getURL('injector.js');
-document.documentElement.appendChild(script);
-script.remove();
+function injectInto(win) {
+  const script = win.document.createElement('script');
+  script.textContent = `
+    (() => {
+      const CUSTOM_SKIN_URL = 'https://YOUR_WORKING_SKIN_LINK';
+      let USED = false;
+
+      console.log('[Miniblox EXT] Injected');
+
+      const desc = Object.getOwnPropertyDescriptor(
+        HTMLImageElement.prototype,
+        'src'
+      );
+
+      Object.defineProperty(HTMLImageElement.prototype, 'src', {
+        set(value) {
+          if (
+            !USED &&
+            typeof value === 'string' &&
+            /skin|player|character|avatar/i.test(value)
+          ) {
+            USED = true;
+            console.log('[Miniblox EXT] ONLY ME skin replaced');
+            return desc.set.call(this, CUSTOM_SKIN_URL);
+          }
+          return desc.set.call(this, value);
+        },
+        get() {
+          return desc.get.call(this);
+        }
+      });
+    })();
+  `;
+  win.document.documentElement.appendChild(script);
+  script.remove();
+}
+
+const interval = setInterval(() => {
+  const iframe = document.querySelector('iframe');
+  if (iframe && iframe.contentWindow && iframe.contentDocument) {
+    clearInterval(interval);
+    injectInto(iframe.contentWindow);
+  }
+}, 500);
